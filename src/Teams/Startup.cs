e6805d1ebb5b648c57355c64bc77d6323a13c6bd
katyakopeyko <kkopeiko@mail.ru>
2020-09-23 16:03:21 +0300

    forgotten line

--- src/Teams/Startup.cs
+++ src/Teams/Startup.cs
@@ -12,6 +12,7 @@ using Teams.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Teams.Data.SingleSelectionQuestionRepos;
 
 namespace Teams
 {
