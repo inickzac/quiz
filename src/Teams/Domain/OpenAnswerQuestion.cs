55dbb381a78f636ad534ade1dcf449b021afc55d
ShortQuality <artiksv2000@gmail.com>
2020-11-10 23:55:44 +0300

    Issues fixed

--- src/Teams/Domain/OpenAnswerQuestion.cs
+++ src/Teams/Domain/OpenAnswerQuestion.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Teams.Data;
-using Teams.Domain;
+﻿using System;
 using Teams.Models;
 
 namespace Teams.Domain
